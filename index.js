31308d39d185ec74fac298b9a1dc597593ce92b2
Kate <frenkel.kat@yandex.ru>
2023-06-15 18:10:11 +0300

    домашка 2.3

--- index.js
+++ index.js
@@ -1,80 +1,16 @@
-const fs = require("fs");
-const path = require("path");
-const axios = require("axios");
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-(async () => {
-  // мы через axios и метод get получаем удаленную запись
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/albums"
-  );
+app.get("/hello/hehe", (req, res) => {
+  return res.json({ firsName: "Home", lastName: "Work" });
+});``
 
-  //  // мы записали данные с json в наш файлик
-  //   fs.writeFile(
-  //     path.resolve(__dirname, "albums.json"),
-  //     JSON.stringify(data),
-  //     "utf-8",
-  //     (err) => {
-  //       if (err) {
-  //         throw err;
-  //       }
-  //       console.log("Успешная запись");
-  //     }
-  //   );
-  //   })();
+app.get("/hello/:name", (request, response) => {
+  request.params.name;
+  return response.json({ message: `Hello, ${request.params.name}` });
+});
 
-  http
-    .createServer((request, response) => {
-      response.setHeader("Content-Type", "text/html; charset=utf-8;");
-
-      if (request.url === "/home" || request.url === "/") {
-        // рабочий вариант, считывает data // response.write(JSON.stringify(data)); 
-        
-
-        fs.readFile(
-          path.resolve(__dirname, "albums.json"),
-          "utf-8",
-          (err, data) => {
-            if (!err) {
-              // страница существует, здесь не хочет считывать data, проблема в области видимости?
-              response.write(JSON.stringify(data));
-              // response.end();
-              // console.log(JSON.stringify(data));
-            } else {
-              // страница не найдена
-              console.log("ошибка");
-            }
-          }
-        );
-
-        // странички с инфой
-        //  https://www.mousedc.ru/learning/485-routing-nodejs/
-        // https://nodejs.org/api/fs.html#fsreadfilepath-options-callback
-
-        // fs.readFile(
-        //   (path.resolve(__dirname, "albums.json")),
-        //   // JSON.stringify(data),
-        //   // "albums.json",
-        //   "utf-8",
-        //   (err, data) => {
-        //     if (err) {
-        //       // console.log('ошибка');
-        //       // return;
-        //       throw err;
-        //     }
-        //     response.write(JSON.stringify(data));
-        //   }
-        // );
-
-        // преобразовать в fs.readFile
-      } else if (request.url == "/about") {
-        response.write("<h2>About</h2>");
-      } else if (request.url == "/contact") {
-        response.write("<h2>Contacts</h2>");
-      } else {
-        response.write("<h2>Not found</h2>");
-      }
-      response.end();
-    })
-    .listen(3000);
-})();
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
